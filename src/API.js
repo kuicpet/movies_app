b640b58df1b443087c8b2f1cb1f53f4a1f1877dc
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-07-16 18:54:49 +0100

    ft--config,helpers,API files added

---
+++ src/API.js
@@ -0,0 +1,43 @@
+import {
+    API_KEY,
+    API_URL,
+    SEARCH_BASE_URL,
+    SEARCH_BASE_URL,
+    POPULAR_BASE_URL,
+    REQUEST_TOKEN_URL,
+    LOGIN_URL,
+    SEARCH_BASE_URL,
+} from "./config";
+
+
+const defaultConfig = {
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json"
+    },
+};
+
+export default {
+    // Fetch Movies
+    fetchMovies: async ( searchItem, page ) => {
+        const endPoint = searchItem
+        ? `${SEARCH_BASE_URL}${searchItem}&page=${page}`
+        : `${POPULAR_BASE_URL}&page=${page}`;
+        return await (await fetch(endPoint)).json();
+    },
+    // Fetch single Movie
+    fetchMovie: async (movieId) => {
+        const endPoint = `${API_URL}/movie/${movieId}?api_key=${API_KEY}`;
+        return await (await fetch(endPoint)).json();
+    },
+    // Fetch credits
+    fetchCredits: async (movieId) => {
+        const creditsEndpoint = `${API_URL}/movie/${movieId}/credits?api_key=${API_KEY}`;
+        return await (await fetch(creditsEndpoint)).json();
+    },
+    // Get token
+    getTokenRequest: async () => {
+        const reqToken = await (await fetch(REQUEST_TOKEN_URL)).json();
+        return reqToken.request_token;
+    }
+}
---
+++ src/config.js
@@ -0,0 +1,31 @@
+// Configuration  
+
+const API_URL = "";
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const SEARCH_BASE_URL = ``
+const POPULAR_BASE_URL = ``
+
+// For Login and Voting
+const REQUEST_TOKEN_URL = ``
+const LOGIN_URL = ``
+const SESSION_ID_URL = ``
+
+// Image url
+const BASE_IMAGE_URL = `http://image.tmdb.org/t/p/`
+
+// BackDrop Image size
+const BACKDROP_SIZE = `W1280`
+
+// Poster Image size
+const POSTER_SIZE = `W780`
+
+export {
+    API_KEY,
+    API_URL,
+    SEARCH_BASE_URL,
+    POPULAR_BASE_URL,
+    REQUEST_TOKEN_URL,
+    LOGIN_URL,
+    SEARCH_BASE_URL,
+}
