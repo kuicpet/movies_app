1ad86f1fce70a941419fb44792b9ee7aa49b1374
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-07-17 02:03:51 +0100

    ft--home page updated with custom hook

--- src/components/Header/Home.js
+++ src/components/Header/Home.js
@@ -1,46 +1,18 @@
 import React, { useState, useEffect } from "react";
-// API
-import API from "../../API";
+
 // CONFIG
 import { POSTER_SIZE, BACKDROP_SIZE, BASE_IMAGE_URL } from "../../config";
 // components
 
 //Hook
-
+import { useHomeFetch } from "../../hooks/useHomeFetch";
 // Images
 import NoImage from "../../images/nathan-dumlao-qDbnNDF2jZ4-unsplash.jpg";
 
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    const fetchMovies = async (page, searchItem = "") => {
-        try {
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(page, searchItem);
-            console.log(movies);
-
-            setState(prev => ({
-                ...movies,
-                results:
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }))
-
-        } catch (error) {
-            setError(true)
-        }
-
-        setLoading(false);
-    };
-
-    // Initial render
-    useEffect(() => {
-        fetchMovies(1)
-    }, [])
+    const { state, loading, error } = useHomeFetch();
+    console.log(state);
 
     return <div>Home Page</div>
 };
