676e3c95b27abec2eb425025bbd582dbb43224d0
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-07-18 13:35:14 +0100

    ft--Breadcrumb component added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const Movie = () => <div>Movie</div>
+//CONFIG
+import { BASE_IMAGE_URL, POSTER_SIZE } from "../config"
+
+//components
+import BreadCrumb from "./BreadCrumb";
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+
+//hook
+import { useMovieFetch } from "../hooks/useMovieFetch";
+
+//Image
+import NoImage from "../images/nathan-dumlao-qDbnNDF2jZ4-unsplash.jpg";
+
+const Movie = () => {
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+    // console.log(movie);
+    if(loading) return <Spinner/>
+    if(error) return <div>Something went wrong ...</div>
+    return (
+        <React.Fragment>
+           <BreadCrumb movieTitle={movie.original_title} />
+        </React.Fragment>
+    )
+}
 
 export default Movie;
